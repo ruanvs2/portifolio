0cddbf130f60e5a5da13b5cdfc0ecd0c9063a810
Ruan Vasconcelos <36383451+ruanvs2@users.noreply.github.com>
2022-06-02 14:55:37 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var burguer = document.querySelector(".burguer")
+
+burguer.addEventListener("click", function(){
+    var sidebar = document.querySelector(".container").classList.toggle("show-menu")
+
+})
